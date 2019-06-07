b2d0d2d0c21a124a8a9ce2a27882bbbaedaad6db
hieuleader <hieuleadergin@gmail.com>
2019-06-07 15:37:31 +0700

    Cac file trong du an

---
+++ home.php
@@ -0,0 +1,37 @@
+<?php
+$title = "Trang chính";
+include 'layouts/header.php';
+?>
+<!-- Full Page Intro -->
+<div class="view full-page-intro" style="background-image: url('img/bg.jpg'); background-repeat: no-repeat; background-size: cover;">
+
+    <!-- Mask & flexbox options-->
+    <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+
+        <!-- Content -->
+        <div class="container">
+
+            <!--Grid row-->
+            <div class="row wow fadeIn">
+
+                <!--Grid column-->
+                <div class="col-md-6 white-text text-center text-md-left">
+                    xin chàoo
+
+                </div>
+                <!--Grid column-->
+
+
+            </div>
+            <!--Grid row-->
+
+        </div>
+        <!-- Content -->
+
+    </div>
+    <!-- Mask & flexbox options-->
+
+</div>
+<!-- Full Page Intro -->
+
+<?php include 'layouts/footer.php'; ?>
